87d0fa066eaa1ba9dac46cbd477bdd0ac54ab4dc
JesseBBuhler <66044271+JesseBBuhler@users.noreply.github.com>
2024-02-23 14:04:51 -0700

    add login and signup pages to frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 
 // pages & components
 import Home from "./pages/Home";
@@ -12,6 +14,8 @@ function App() {
         <div className="pages">
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
           </Routes>
         </div>
       </BrowserRouter>
