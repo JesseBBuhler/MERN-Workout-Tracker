a857ff278ab63a088498fad1945f6b538cd89c1f
JesseBBuhler <66044271+JesseBBuhler@users.noreply.github.com>
2024-02-23 09:19:40 -0700

    add email validation

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
+const validator = require("validator");
 
 const Schema = mongoose.Schema;
 
@@ -18,10 +19,23 @@ const userSchema = new Schema({
 // static signup method
 // cannot use arrow function in functions that use "this"
 userSchema.statics.signup = async function (email, password) {
+  //validation
+  if (!email || !password) {
+    throw Error("All fields must be filled");
+  }
+
   const exists = await this.findOne({ email });
 
   if (exists) {
-    throw Error("Email already in use");
+    throw Error("Email is already in use");
+  }
+
+  if (!validator.isEmail(email)) {
+    throw Error("Email is not valid");
+  }
+
+  if (!validator.isStrongPassword(password)) {
+    throw Error("Password is not strong enough");
   }
 
   const salt = await bcrypt.genSalt(10);
