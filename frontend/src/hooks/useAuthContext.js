d9a4fa325c2497703c87d99e5bfbb83685a4f378
JesseBBuhler <66044271+JesseBBuhler@users.noreply.github.com>
2024-02-25 11:34:04 -0700

    implement login and logout

--- frontend/src/hooks/useAuthContext.js
+++ frontend/src/hooks/useAuthContext.js
@@ -1,7 +1,7 @@
 import { AuthContext } from "../context/AuthContext";
 import { useContext } from "react";
 
-const useAuthContext = () => {
+export const useAuthContext = () => {
   const context = useContext(AuthContext);
 
   if (!context) {
