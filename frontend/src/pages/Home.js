02412df0762d46dc07e8c42bcd6b49f0e3930ca8
JesseBBuhler <66044271+JesseBBuhler@users.noreply.github.com>
2024-02-27 20:28:32 -0700

    authorize front end api requests

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
+import useAuthContext from "../hooks/useAuthContext";
 
 //components
 import WorkoutDetails from "../components/WorkoutDetails";
@@ -7,19 +8,30 @@ import WorkoutForm from "../components/WorkoutForm";
 
 const Home = () => {
   const { workouts, dispatch } = useWorkoutsContext();
+  const { user } = useAuthContext();
   //[] tells it to only fire once when the component is rendered
-  useEffect(() => {
-    //do not make the parameter function of useEffect async. make it the inner fucntion instead
-    const fetchWorkouts = async () => {
-      const response = await fetch("/api/workouts");
-      const json = await response.json();
-      if (response.ok) {
-        dispatch({ type: "SET_WORKOUTS", payload: json });
-      }
-    };
+  useEffect(
+    () => {
+      //do not make the parameter function of useEffect async. make it the inner fucntion instead
+      const fetchWorkouts = async () => {
+        const response = await fetch("/api/workouts", {
+          headers: {
+            Authorization: `Bearer ${user.token}`,
+          },
+        });
+        const json = await response.json();
+        if (response.ok) {
+          dispatch({ type: "SET_WORKOUTS", payload: json });
+        }
+      };
 
-    fetchWorkouts();
-  }, [dispatch]);
+      if (user) {
+        fetchWorkouts();
+      }
+    },
+    [dispatch],
+    user
+  );
   return (
     <div className="home">
       <div className="workouts">
